009d25f5d8f7b2e26fa53a6331064d37d6870ea8
Noelle Mostert <34947553+NoelleMostert@users.noreply.github.com>
2018-02-14 22:26:43 +0200

    Update ex01day02.c

--- ex01day02.c
+++ ex01day02.c
@@ -1,2 +1,24 @@
 # BCDay02
 We think code bootcamp day02
+#include <unistd.h>
+
+int ft_putchar(char c)
+{
+    write(1, &c, 1);
+    return (0);
+}
+void ft_alphabet(void)
+{
+    char letter;
+    letter = 'z';
+    while (letter >= 'a')
+    {
+        ft_putchar(letter);
+        letter--;
+    }
+}
+int main()
+{
+    ft_alphabet();
+    return(0);
+}
